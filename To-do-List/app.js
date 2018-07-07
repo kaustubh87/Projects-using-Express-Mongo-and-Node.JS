ffd709ca6b0659b045705121f386e3931e13bf9e
kvin1087 <kaustubh87@gmail.com>
2018-07-07 10:49:29 -0400

    Added MongoDb Connection

--- To-do-List/app.js
+++ To-do-List/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/todoapp';
+
 const port = 8089;
 
 // Body Parser Middleware
@@ -14,6 +18,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
+
+// Connect to mongodb
+
+MongoClient.connect(url, (err, database) => {
+    console.log('MongoDb Connected . . .');
+    if(err) throw err;
+    
+});
 
 app.get('/', (req, res, next) => {
     res.render('index');
