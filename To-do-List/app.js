76d5ee7ef6c605389d84da7b3d83b48a5a7bc6d4
kvin1087 <kaustubh87@gmail.com>
2018-07-07 08:55:03 -0400

    Installed dependencies

--- To-do-List/app.js
+++ To-do-List/app.js
@@ -15,6 +15,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
 app.listen(port, () => {
     console.log('Server running on ' +port);
 });
