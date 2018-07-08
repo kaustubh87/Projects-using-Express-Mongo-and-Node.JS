61cea70b87014166df3347ce6c17273fb3bc82ba
kvin1087 <kaustubh87@gmail.com>
2018-07-08 09:52:35 -0400

    got the delete id then sent a delete request through ajax

---
+++ To-do-List/public/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    $('.delete-todo').on('click', (event) =>{
+       $target = $(event.target);
+       const id = $target.attr('data-id');
+       
+       $.ajax({
+           type: 'DELETE',
+           url: '/todo/delete/' +id,
+           success: (response) => {
+               alert('Deleting Tod');
+               window.location.href= "/";
+           },
+           error: (error) => {
+               console.log(error);
+           }
+       })
+
+    });
+});
