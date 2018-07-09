5caed69c87ce68538df492b0e608b9644bd6e7f2
kvin1087 <kaustubh87@gmail.com>
2018-07-09 09:05:27 -0400

    Added custom js

--- To-do-List/public/js/main.js
+++ To-do-List/public/js/main.js
@@ -1,19 +1,18 @@
 $(document).ready(() => {
-    $('.delete-todo').on('click', (event) =>{
-       $target = $(event.target);
+    $('.delete-todo').on('click', (e) =>{
+       $target = $(e.target);
        const id = $target.attr('data-id');
-       
+       console.log(id);
        $.ajax({
            type: 'DELETE',
            url: '/todo/delete/' +id,
            success: (response) => {
-               alert('Deleting Tod');
+               alert('Deleting Todo');
                window.location.href= "/";
            },
            error: (error) => {
                console.log(error);
            }
-       })
-
+       });
     });
 });
