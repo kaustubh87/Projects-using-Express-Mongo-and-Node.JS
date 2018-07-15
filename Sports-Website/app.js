f6484f01db3c7d744808126be3bdd9741be058ca
kvin1087 <kaustubh87@gmail.com>
2018-07-15 10:13:37 -0400

    Added basic variables

---
+++ Sports-Website/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
