0c60ca14aafb95bcaacf6cd5836ffc17f02c28fc
kvin1087 <kaustubh87@gmail.com>
2018-07-15 10:21:02 -0400

    Added connect flash module

--- Sports-Website/app.js
+++ Sports-Website/app.js
@@ -4,3 +4,19 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
+
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('connect-flash')());
+
+app.use(function(req, res, next){
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
