9f31bd0ca515288a046df7a656d3cf8a2863b522
kvin1087 <kaustubh87@gmail.com>
2018-07-16 10:30:58 -0400

    Added express validator

--- Sports-Website/app.js
+++ Sports-Website/app.js
@@ -20,3 +20,16 @@ app.use(function(req, res, next){
     res.locals.messages = require('express-messages')(req, res);
     next();
 });
+
+// Express Validator
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        var namespace = param.split('.') , root = namespace.shift() , formParam = root;
+    }
+
+    while(namespace.length) {
+        
+    }
+
+}));
